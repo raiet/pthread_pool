13dc0766cee85528963914610dec6226fde9520f
raiet <hustraiet@gmail.com>
2014-03-09 21:43:47 +0800

    Create pool_test.c
    线程池的测试用例

---
+++ pool_test.c
@@ -0,0 +1,31 @@
+#include "thread_pool.h"
+
+
+void my_process(void* args){
+	tp_work_arg *arg=(tp_work_arg*)args;
+	int val=*(int*)arg->args;
+	printf("hello :%d\n",val);
+	sleep(3);
+	printf("%d exit!!\n",val);
+}
+
+int main(){
+	int num=100;
+	int workingnum[100];
+	tp_thread_pool *pool=creat_thread_pool(5,20);
+	
+
+	tp_work *worker=(tp_work*)malloc(sizeof(tp_work));
+	worker->process_job=my_process;
+	
+	int i;
+    for (i = 0; i < num; i++){			
+        tp_work_arg *arg=(tp_work_arg *)malloc(sizeof(tp_work_arg));
+		workingnum[i]=i;
+        arg->args=(void*)&workingnum[i];
+		tp_add_job(pool,worker,arg);
+     }
+
+    sleep(60);
+}
+
